5767b9075e1e01a0d27a16be6d4914c595614673
Dastnn <code.geometric756@passmail.com>
2025-11-24 11:37:49 -0700

    -Fps added

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import pygame
-
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+
 
 def main():
-    print("Starting Asteroids with pygame version: VERSION")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fpsClock = pygame.time.Clock()
+    dt = 0
+    
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+                      
+        screen.fill("black")
+        pygame.display.flip()
+        dt = fpsClock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
